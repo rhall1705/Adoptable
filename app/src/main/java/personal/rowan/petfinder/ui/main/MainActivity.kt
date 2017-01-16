ab121bc4d9706c7440d68ca9c6712561b77dca8a
Rowan Hall <rhall1705@gmail.com>
2017-01-16 16:41:18 -0500

    Added bottom navigation UI (no functionality yet) and assets

--- app/src/main/java/personal/rowan/petfinder/ui/main/MainActivity.kt
+++ app/src/main/java/personal/rowan/petfinder/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package personal.rowan.petfinder.ui.main
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import butterknife.bindView
 
 import personal.rowan.petfinder.R
 import personal.rowan.petfinder.ui.base.BaseActivity
@@ -8,14 +11,31 @@ import personal.rowan.petfinder.ui.petmaster.container.PetMasterContainerFragmen
 
 class MainActivity : BaseActivity() {
 
+    val bottomNavigation: BottomNavigationView by bindView(R.id.main_bottom_navigation)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_container, PetMasterContainerFragment.getInstance())
-                    .commit()
+            replaceFragmentInContainer(PetMasterContainerFragment.getInstance())
         }
+
+        bottomNavigation.setOnNavigationItemSelectedListener({
+            menuItem ->
+            when(menuItem.itemId) {
+                R.id.action_nearby_animals -> showToastMessage("Navigate to pets page")
+                R.id.action_nearby_shelters -> showToastMessage("Navigate to shelters page")
+                R.id.action_search -> showToastMessage("Navigate to search page")
+            }
+            true
+        })
     }
+
+    private fun replaceFragmentInContainer(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit()
+    }
+
 }
