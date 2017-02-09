27b5f5c275a74f33059a81d6dda7982fe26e383b
Rowan Hall <rhall1705@gmail.com>
2017-02-09 00:04:51 -0500

    Groundwork for pet detail

---
+++ app/src/main/java/personal/rowan/petfinder/ui/pet/detail/PetDetailActivity.kt
@@ -0,0 +1,15 @@
+package personal.rowan.petfinder.ui.pet.detail
+
+import personal.rowan.petfinder.ui.base.BaseFragment
+import personal.rowan.petfinder.ui.base.ContainerActivity
+
+/**
+ * Created by Rowan Hall
+ */
+class PetDetailActivity : ContainerActivity() {
+
+    override fun getFragment(): BaseFragment? {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ app/src/main/java/personal/rowan/petfinder/ui/pet/detail/PetDetailFragment.kt
@@ -0,0 +1,12 @@
+package personal.rowan.petfinder.ui.pet.detail
+
+import personal.rowan.petfinder.ui.base.BaseFragment
+
+/**
+ * Created by Rowan Hall
+ */
+class PetDetailFragment : BaseFragment() {
+
+
+
+}
